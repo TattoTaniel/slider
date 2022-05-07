7ceed375ebfab781b0d703f8c20bcc9c6f77780a
Lyzo Tatyana <avengerdecade6@gmail.com>
2022-05-07 22:59:31 +0200

    dkuh

---
+++ main.js
@@ -0,0 +1,33 @@
+// const object = [
+//     {
+//         title: '',
+//         text: '',
+//     },
+//     {
+//         title: '',
+//         text: '',
+//     },
+//     {
+//         title: '',
+//         text: '',
+//     },
+
+// ]
+
+// const div = document.querySelector('div');
+
+// articles.forEach(article => {
+//     const section = document.createElement('section');
+
+//     section.innerHTML = `
+//     <h2>${articles.title}</h2>
+//     <p>${articles.title}</p>`
+
+//     div.appendChild(section);
+
+// })
+
+// divTest.addEventListener('click', () => {
+//     divTest.classList.toggle('active')
+// })
+

6078fcf1e7c019eb38e3b86f49769497e9632c21
Lyzo Tatyana <avengerdecade6@gmail.com>
2022-05-07 23:01:02 +0200

    dkuh

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
-// const object = [
-//     {
-//         title: '',
-//         text: '',
-//     },
-//     {
-//         title: '',
-//         text: '',
-//     },
-//     {
-//         title: '',
-//         text: '',
-//     },
+const articles = [
+    {
+        title: 'This is freedom',
+        image: 'https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80',
 
-// ]
+    },
 
-// const div = document.querySelector('div');
+    {
+        title: 'This is brething',
+        image: 'https://images.unsplash.com/photo-1490750967868-88aa4486c946?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    },
 
-// articles.forEach(article => {
-//     const section = document.createElement('section');
+    {
+        title: 'This is love',
+        image: 'https://images.unsplash.com/photo-1603955389958-8ab4c2025b71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    }
+]
 
-//     section.innerHTML = `
-//     <h2>${articles.title}</h2>
-//     <p>${articles.title}</p>`
+const img = document.querySelector('img');
+const h1 = document.querySelector('h1');
 
-//     div.appendChild(section);
+let active = 0;
 
-// })
+img.src = articles[active].image;
+h1.textContent = articles[active].title;
 
-// divTest.addEventListener('click', () => {
-//     divTest.classList.toggle('active')
-// })
+const changeSlide = () => {
+    active++;
 
+    if (active === articles.length) {
+        active = 0;
+    }
+
+    img.src = articles[active].image;
+    h1.textContent = articles[active].title;
+
+}
+
+setInterval(changeSlide, 2000);
